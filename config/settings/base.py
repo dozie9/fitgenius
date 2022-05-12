6a2947b23ddef870d9802c19dc00da9935d406f7
dozie9 <dozie.steve@gmail.com>
2022-05-12 19:05:56 +0100

    remove STATICFILES_FINDERS

--- config/settings/base.py
+++ config/settings/base.py
@@ -158,10 +158,10 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [str(APPS_DIR / "static")]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
+# STATICFILES_FINDERS = [
+#     "django.contrib.staticfiles.finders.FileSystemFinder",
+#     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+# ]
 
 # MEDIA
 # ------------------------------------------------------------------------------
